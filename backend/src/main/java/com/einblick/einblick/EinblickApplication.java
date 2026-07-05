ee99b214dca2146c8414081d63f9884de897af84
Koalaman0 <jiwon0922@gmail.com>
2026-07-05 17:18:25 +0900

    Initial commit

---
+++ backend/src/main/java/com/einblick/einblick/EinblickApplication.java
@@ -0,0 +1,13 @@
+package com.einblick.einblick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinblickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EinblickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/einblick/einblick/EinblickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einblick.einblick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinblickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
